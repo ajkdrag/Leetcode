0a1839185a88c6151fb883d79f9d05ba12b6385a
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-16 11:33:15 +0530

    Create Search_in_a_BST.java

---
+++ Search_in_a_BST.java
@@ -0,0 +1,25 @@
+/*
+  Problem at : https://leetcode.com/problems/search-in-a-binary-search-tree/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val)
+            return root;
+        else if(root.val > val)
+            return searchBST(root.left, val);
+        else
+            return searchBST(root.right, val);
+    }
+}
+
+
