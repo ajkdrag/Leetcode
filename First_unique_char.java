e4ab2ce0eda62390ebcb30f7f10018c6389a4f35
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-27 09:26:05 +0530

    Update First_unique_char.java

--- First_unique_char.java
+++ First_unique_char.java
@@ -31,4 +31,43 @@ class Solution {
     }
 }
 
+// Here is another bootlegged solution that scans through the array only once but runtime is very poor
+// even though the time complexity isn't.
+class Solution {
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> set = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        if(n == 0)
+            return -1;
+        q.add(0);
+        set.put(arr[0], 1);
+        for(int i = 1; i < n; ++i){
+            char c = arr[i];
+            if(!q.isEmpty() && arr[q.peek()] == c){
+                q.poll();
+                set.put(c, set.get(c) + 1);
+                continue;
+            }
+            
+            if(set.containsKey(c)){
+                set.put(c, set.get(c) + 1);
+                continue;
+            }
+            set.put(c, 1);
+            q.offer(i);
+        }
+        while(!q.isEmpty()){
+            if(set.getOrDefault(arr[q.peek()], 0) > 1)
+               q.poll();
+            else
+               break;
+        }
+        return q.isEmpty() ? -1 : q.poll();
+    }
+}
+
+
+
 
