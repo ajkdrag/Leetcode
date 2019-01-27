52bf2c1d270014b150116d0fc9925bb6cc840262
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-27 18:13:28 +0530

    Create Same_tree.java

---
+++ Same_tree.java
@@ -0,0 +1,17 @@
+/*
+  Problem at : https://leetcode.com/problems/same-tree/
+*/
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+        if(p == null || q == null)
+            return false;
+        if(p.val == q.val)
+            return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}
+
+
