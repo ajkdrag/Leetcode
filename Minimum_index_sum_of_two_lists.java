b9149f3274a08e968f2e1dcaba52743f9adea18f
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-29 08:48:36 +0530

    Create Minimum_index_sum_of_two_lists.java

---
+++ Minimum_index_sum_of_two_lists.java
@@ -0,0 +1,44 @@
+/*
+  Problem at : https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+*/
+
+// Non HashMap solution
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        List < String > res = new ArrayList < > ();
+        for (int sum = 0; sum < list1.length + list2.length - 1; sum++) {
+            for (int i = 0; i <= sum; i++) {
+                if (i < list1.length && sum - i < list2.length && list1[i].equals(list2[sum - i]))
+                    res.add(list1[i]);
+            }
+            if (res.size() > 0)
+                break;
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
+
+// HashMap Solution
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+          HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List < String > res = new ArrayList < > ();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
+
+
