7362759ef1f49cb9f79dad2caa08061b74acc18f
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-29 08:52:27 +0530

    Create Diameter_of_binary_tree.java

---
+++ Diameter_of_binary_tree.java
@@ -0,0 +1,22 @@
+/*
+  Problem at : https://leetcode.com/problems/diameter-of-binary-tree/
+  Solution at  : https://leetcode.com/problems/diameter-of-binary-tree/solution/
+*/
+
+class Solution {
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+    public int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L+R+1);
+        return Math.max(L, R) + 1;
+    }
+}
+
+
