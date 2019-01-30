88c752154cf115d21a3ce2eaf65342cd35cdd083
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-01-30 09:31:43 +0530

    Create Rectangle_overlap.java

---
+++ Rectangle_overlap.java
@@ -0,0 +1,10 @@
+/*
+  Problem at : https://leetcode.com/problems/rectangle-overlap/
+*/
+
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return (rec1[1] <  rec2[3] && rec1[0] < rec2[2] && rec1[2] > rec2[0] && rec1[3] > rec2[1]);
+    }
+}
+
