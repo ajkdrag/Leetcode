55b06023f19b6b9eacfd2e367e8a4ff33076502d
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-02 09:31:25 +0530

    Create Find_smallest_letter_greater_than_target.java

---
+++ Find_smallest_letter_greater_than_target.java
@@ -0,0 +1,17 @@
+/*
+  Problem at : https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+*/
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0, hi = letters.length;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (letters[mi] <= target) lo = mi + 1;
+            else hi = mi;
+        }
+        return letters[lo % letters.length];
+    }
+}
+
+
