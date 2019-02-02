b0589800aa5af5d7ac2ca7ed21bb295931705522
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-02 20:02:12 +0530

    Create Kth_largest_in_a_stream.java

---
+++ Kth_largest_in_a_stream.java
@@ -0,0 +1,29 @@
+/*
+  Problem at : https://leetcode.com/problems/kth-largest-element-in-a-stream/
+*/
+
+// Priority Queue soln.
+class KthLargest {
+    final PriorityQueue<Integer> q;
+    final int k;
+
+    public KthLargest(int k, int[] a) {
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for (int n : a)
+            add(n);
+    }
+
+    public int add(int n) {
+        if (q.size() < k)
+            q.offer(n);
+        else if (q.peek() < n) {
+            q.poll();
+            q.offer(n);
+        }
+        return q.peek();
+    }
+}
+    
+    
+  
