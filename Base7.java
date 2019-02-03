5fc6faa8f677a4c8bfe6973ba666a880fbaa2750
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-03 22:26:09 +0530

    Create Base7.java

---
+++ Base7.java
@@ -0,0 +1,15 @@
+/*
+  Problem at : https://leetcode.com/problems/base-7/
+*/
+
+class Solution {
+    public String convertToBase7(int num) {
+        if (num < 0)
+            return '-' + convertToBase7(-num);
+        if (num < 7)
+            return num + "";
+        return convertToBase7(num / 7) + num % 7;
+    }
+}
+
+
