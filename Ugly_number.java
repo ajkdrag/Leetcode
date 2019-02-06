2f638d788cf05d1728f8bac8dd8e56c119586d10
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-06 09:03:08 +0530

    Create Ugly_number.java

---
+++ Ugly_number.java
@@ -0,0 +1,22 @@
+/*
+  Problem at : https://leetcode.com/problems/ugly-number/
+*/
+
+class Solution {
+    public boolean isUgly(int num) {
+        if(num <= 0)
+            return false;
+        while((num & 1) == 0){
+            num >>>= 1;
+        }
+        while(num % 3 == 0){
+            num/= 3;
+        }
+        while(num % 5 == 0){
+            num/= 5;
+        }
+        return num == 1;
+    }
+}
+
+
