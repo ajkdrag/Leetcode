590998fdb71d97a15cd607595486b14751a8c67b
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-07 08:33:13 +0530

    Create Implement_queue_using_stacks.java

---
+++ Implement_queue_using_stacks.java
@@ -0,0 +1,47 @@
+/*
+  Problem at : https://leetcode.com/problems/implement-queue-using-stacks/
+*/
+
+class MyQueue {
+    
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+    private int front;
+    
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if (s1.empty())
+            front = x;
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (s2.empty()) {
+            while (!s1.empty())
+                s2.push(s1.pop());
+        }
+        return s2.pop();   
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if (!s2.empty()) {
+            return s2.peek();
+        }
+        return front;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+
