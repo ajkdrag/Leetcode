303eb2849e1e8c36f0c83f06b17302b8770992df
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-09 09:23:14 +0530

    Create Valid_mountain_array.java

---
+++ Valid_mountain_array.java
@@ -0,0 +1,19 @@
+/*
+  Problem at : https://leetcode.com/problems/valid-mountain-array/
+*/
+
+class Solution {
+    public boolean validMountainArray(int[] A) {
+        int N = A.length;
+        int i = 0;
+        while (i+1 < N && A[i] < A[i+1])
+            i++;
+        if (i == 0 || i == N-1)
+            return false;
+        while (i+1 < N && A[i] > A[i+1])
+            i++;
+        return i == N-1;
+    }
+}
+
+
