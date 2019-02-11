2a5d0bd6f08f0ec343a5563277098ac19a1e9dfe
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-11 09:40:37 +0530

    Update Number_of_segments_in_a_string.java

--- Number_of_segments_in_a_string.java
+++ Number_of_segments_in_a_string.java
@@ -2,10 +2,24 @@
   Problem at : https://leetcode.com/problems/number-of-segments-in-a-string/
 */
 
+// regex
 class Solution {
     public int countSegments(String s) {
         s = "# " + s;
         return -1  + s.split("\\s+").length;
+    }
+}
+
+// iterative
+class Solution {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+        return segmentCount;
     }
 }
 
