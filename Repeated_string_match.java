13b2ddced3d6443b5f9ace638bcc58cfd71ac8ec
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-02-19 13:48:10 +0530

    Create Repeated_string_match.java

---
+++ Repeated_string_match.java
@@ -0,0 +1,17 @@
+/*
+  Problem at : https://leetcode.com/problems/repeated-string-match/
+  Solution at : https://leetcode.com/problems/repeated-string-match/solution/
+*/
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        int q = 1;
+        StringBuilder S = new StringBuilder(A);
+        for (; S.length() < B.length(); q++) S.append(A);
+        if (S.indexOf(B) >= 0) return q;
+        if (S.append(A).indexOf(B) >= 0) return q+1;
+        return -1;
+    }
+}
+
+
