d79196e20acad30cf7b089691a1032e1ccdbaed1
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-03-30 11:11:50 +0530

    Create Rotate_array.java

---
+++ Rotate_array.java
@@ -0,0 +1,45 @@
+/*
+  Problem at : https://leetcode.com/problems/rotate-array/
+  Solution at : https://leetcode.com/problems/rotate-array/solution/
+*/
+
+// by reversing
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+// by cyclic replacement
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+}
+
+
