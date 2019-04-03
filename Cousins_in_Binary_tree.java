724e8a7a371ce18bb0b1a9767695eee4b2d91810
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-04-03 11:03:54 +0530

    Create Cousins_in_Binary_tree.java

---
+++ Cousins_in_Binary_tree.java
@@ -0,0 +1,25 @@
+/*
+  Problem at : https://leetcode.com/problems/cousins-in-binary-tree/
+*/
+
+class Solution {
+    HashMap<Integer, Integer> depth;
+    HashMap<Integer, TreeNode> parent;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        depth = new HashMap();
+        parent = new HashMap();
+        dfs(root, null);
+        return (depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y));
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            depth.put(node.val, par != null ? 1 + depth.get(par.val) : 0);
+            parent.put(node.val, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
+        }
+    }
+}
+
+
