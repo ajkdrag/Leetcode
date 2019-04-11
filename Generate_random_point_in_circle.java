4aeb65c762952e7e4d69a68fa035789a7ed631c9
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-04-11 14:24:46 +0530

    Create Generate_random_point_in_circle.java

---
+++ Generate_random_point_in_circle.java
@@ -0,0 +1,22 @@
+/*
+  Problem at : https://leetcode.com/problems/generate-random-point-in-a-circle/
+*/
+
+class Solution {
+    double radius, x_center, y_center;
+    public Solution(double radius, double x_center, double y_center) {
+        this.radius=radius;
+        this.x_center=x_center;
+        this.y_center=y_center;
+    }
+    
+    public double[] randPoint() {
+        double len= Math.sqrt(Math.random())*radius;
+        double deg= Math.random()*2*Math.PI;
+        double x= x_center+len*Math.cos(deg);
+        double y= y_center+len*Math.sin(deg);
+        return new double[]{x,y};
+    }
+}
+
+
